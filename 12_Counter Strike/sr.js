20c19c4b553c55ebdbec2ad2a8e5dfc862669ba1
teameviral <dxpubg61@gmail.com>
2023-03-13 22:37:19 +0530

    first commit

---
+++ 12_Counter Strike/sr.js
@@ -0,0 +1,22 @@
+let typeData = new Typed(".role" , {
+    strings:[
+        "Counter Strike 1.6",
+        "TecHETC 2k23"
+    ],
+    loop:true,
+    typeSpeed: 120,
+    backDelay: 4000,
+    backSpeed: 70,
+    })
+    let typeData1 = new Typed(".role1" , {
+        strings:[
+            "Case, Capsule, Kit, Oh My!",
+            "version 8.0"
+        ],
+        loop:true,
+        typeSpeed: 115,
+        backDelay: 3300,
+        backSpeed: 70,
+        })
+        let audio=new Audio("Counter Strike audio.mp3");
+        audio.play();
